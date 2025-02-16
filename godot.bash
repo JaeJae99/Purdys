8c77f47967415f92534c3bcd74d549fe4cb6917a
JonahBigJ <jonah.d.stanley@gmail.com>
2025-02-16 10:58:28 -0500

    changed bash file

--- godot.bash
+++ godot.bash
@@ -1,3 +1,6 @@
 #basic bash script to run the godot project
-/home/jonah/Godot/Godot_v4.3-stable_linux.x86_64 -e
 
+tmux rename-window 'nvim'
+tmux new-window -n "git"
+tmux new-window -n "godot": '/home/jonah/Godot/Godot_v4.3-stable_linux.x86_64 -e' 
+nvim
